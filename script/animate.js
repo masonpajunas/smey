fe7f43e0fec339bf82d915cabe9e533c21ec8a2a
Mason <122550933+masonpajunas@users.noreply.github.com>
2024-10-25 10:27:47 -0400

    Update animate.js

--- script/animate.js
+++ script/animate.js
@@ -2,90 +2,239 @@ window.addEventListener("DOMContentLoaded", (event) => {
   // Split text into spans
   let typeSplit = new SplitType("[text-split]", {
     types: "words, chars",
-    tagName: "span"
+    tagName: "span",
   });
 
-  // Link timelines to scroll position
-  function createScrollTrigger(triggerElement, timeline) {
+  // Updated function to accept different start positions
+  function createScrollTrigger(triggerElement, timeline, startPosition) {
     // Reset tl when scroll out of view past bottom of screen
     ScrollTrigger.create({
       trigger: triggerElement,
       start: "top bottom",
-      onLeaveBack: () => {
-        timeline.progress(0);
-        timeline.pause();
-      }
     });
-    // Play tl when scrolled into view (60% from top of screen)
+    // Play tl when scrolled into view with a variable start position
     ScrollTrigger.create({
       trigger: triggerElement,
-      start: "top 60%",
-      onEnter: () => timeline.play()
+      start: `top ${startPosition}`,
+      onEnter: () => timeline.play(),
     });
   }
 
   $("[words-slide-up]").each(function (index) {
     let tl = gsap.timeline({ paused: true });
-    tl.from($(this).find(".word"), { opacity: 0, yPercent: 100, duration: 0.5, ease: "back.out(2)", stagger: { amount: 0.5 } });
-    createScrollTrigger($(this), tl);
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      yPercent: 100,
+      duration: 0.5,
+      ease: "back.out(1)",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "75%");
   });
 
-  $("[words-rotate-in]").each(function (index) {
+  $("[words-slide-up-less]").each(function (index) {
     let tl = gsap.timeline({ paused: true });
-    tl.set($(this).find(".word"), { transformPerspective: 1000 });
-    tl.from($(this).find(".word"), { rotationX: -90, duration: 0.6, ease: "power2.out", stagger: { amount: 0.6 } });
-    createScrollTrigger($(this), tl);
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      yPercent: 100,
+      duration: 0.5,
+      ease: "back.out(1)",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "90%");
+  });
+
+  $("[words-slide-from-left]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      x: "-1em",
+      duration: 0.6,
+      ease: "power2.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "75%");
+  });
+
+  $("[words-slide-from-left-further]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      x: "-1em",
+      duration: 0.4,
+      ease: "power2.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "80%");
+  });
+
+  $("[all-slide-from-right]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      x: "1em",
+      duration: 0.6,
+      ease: "power2.out",
+    });
+    createScrollTrigger($(this), tl, "90%");
+  });
+
+  $("[words-slide-from-right-less]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      x: "1em",
+      duration: 0.6,
+      ease: "power2.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "60%");
   });
 
   $("[words-slide-from-right]").each(function (index) {
     let tl = gsap.timeline({ paused: true });
-    tl.from($(this).find(".word"), { opacity: 0, x: "1em", duration: 0.6, ease: "power2.out", stagger: { amount: 0.2 } });
-    createScrollTrigger($(this), tl);
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      x: "1em",
+      duration: 0.6,
+      ease: "power2.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "75%");
   });
 
-$("[letters-slide-up]").each(function (index) {
-  let tl = gsap.timeline({ paused: true });
-  tl.from($(this).find(".char"), {
-    yPercent: 50,
-    opacity: 0,
-    scale: 0.9,
-    duration: 0.2,
-    ease: "power1.out",
-    stagger: { amount: 0.2 }
+  $("[words-slide-from-right-further]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      x: "1em",
+      duration: 0.4,
+      ease: "power2.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "80%");
   });
-  createScrollTrigger($(this), tl);
-});
+
+  $("[letters-slide-from-right-bottom]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      opacity: 0,
+      x: "1em",
+      duration: 0.6,
+      ease: "power2.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "100%");
+  });
+
+  $("[letters-slide-from-right]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      opacity: 0,
+      x: "1em",
+      duration: 0.6,
+      ease: "power2.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "75%");
+  });
+
+  $("[letters-slide-from-right-less]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      opacity: 0,
+      x: "0.25em",
+      duration: 0.6,
+      ease: "power2.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "60%");
+  });
+
+  $("[letters-slide-up-delay-200]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      yPercent: 50,
+      opacity: 0,
+      duration: 0.2,
+      delay: 0.2, // 200ms delay
+      ease: "power1.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "90%");
+  });
+
+  $("[letters-slide-up]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      yPercent: 50,
+      opacity: 0,
+      duration: 0.2,
+      ease: "power1.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "75%");
+  });
+
+  $("[letters-slide-up-less]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      yPercent: 50,
+      duration: 0.2,
+      ease: "power1.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "60%");
+  });
+
+  // Second timeline with a different trigger position
+  $("[letters-slide-up-further]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      yPercent: 50,
+      opacity: 0,
+      scale: 0.9,
+      duration: 0.2,
+      ease: "power1.out",
+      stagger: { amount: 0.2 },
+    });
+    createScrollTrigger($(this), tl, "90%");
+  });
 
   $("[letters-slide-down]").each(function (index) {
     let tl = gsap.timeline({ paused: true });
-    tl.from($(this).find(".char"), { yPercent: -120, duration: 0.3, ease: "power1.out", stagger: { amount: 0.7 } });
+    tl.from($(this).find(".char"), {
+      yPercent: -120,
+      duration: 0.3,
+      ease: "power1.out",
+      stagger: { amount: 0.7 },
+    });
     createScrollTrigger($(this), tl);
   });
 
   $("[letters-fade-in]").each(function (index) {
     let tl = gsap.timeline({ paused: true });
-    tl.from($(this).find(".char"), { opacity: 0, duration: 0.2, ease: "power1.out", stagger: { amount: 0.8 } });
+    tl.from($(this).find(".char"), {
+      opacity: 0,
+      duration: 0.2,
+      ease: "power1.out",
+      stagger: { amount: 0.8 },
+    });
     createScrollTrigger($(this), tl);
   });
 
   $("[letters-fade-in-random]").each(function (index) {
     let tl = gsap.timeline({ paused: true });
-    tl.from($(this).find(".char"), { opacity: 0, duration: 0.05, ease: "power1.out", stagger: { amount: 0.4, from: "random" } });
-    createScrollTrigger($(this), tl);
-  });
-
-  $("[scrub-each-word]").each(function (index) {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: $(this),
-        start: "top 90%",
-        end: "top center",
-        scrub: true
-      }
+    tl.from($(this).find(".char"), {
+      opacity: 0,
+      duration: 0.05,
+      ease: "power1.out",
+      stagger: { amount: 0.4, from: "random" },
     });
-    tl.from($(this).find(".word"), { opacity: 0.2, duration: 0.2, ease: "power1.out", stagger: { each: 0.4 } });
+    createScrollTrigger($(this), tl, "75%");
   });
 
   // Avoid flash of unstyled content
   gsap.set("[text-split]", { opacity: 1 });
 });
+
